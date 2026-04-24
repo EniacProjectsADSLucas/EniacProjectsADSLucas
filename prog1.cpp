b60320355fd0d8d80d14f7b1f9088f4b1244a6cb
EniacProjectsADSLucas <211912026@eniac.edu.br>
2026-04-24 07:58:36 -0300

    Add files via upload

---
+++ prog1.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h> //Biblioteca para usar o printf
+#include<stdlib.h> //Biblioteca para usar a fumção system
+int main()
+{
+    printf("Hello World!\n");//imprime(exibe) a mensagem na tela
+    system("pause");//Pausa a execução (Windows)
+    return 0; //Finaliza o programa
+}
